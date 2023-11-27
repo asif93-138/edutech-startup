53fb5072acbbbfd5e3f20125ac668af25da18cac
Minhaz Chowdhury <chowdhuryminhaz021@gmail.com>
2023-11-27 20:49:49 +0600

    global fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import HomePage from "./Components/HomePage/Home";
+import "./styles/App.css";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <HomePage />
