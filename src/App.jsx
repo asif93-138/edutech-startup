294071a45e507aaff5027f3a4382c88d2b80c333
Minhaz Chowdhury <chowdhuryminhaz021@gmail.com>
2023-11-29 01:34:50 +0600

    quizpage rebuilt

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import reactLogo from "./assets/react.svg";
 import CourseList from "./Components/Course_List/CourseList";
 import "./styles/App.css";
 import SignUp from "./Components/SignUp/signup";
+import Quiz from "./Components/Quiz/Quiz";
 function App() {
   return (
     <div>
-      <SignUp />
+      <Quiz />
     </div>
   );
 }
